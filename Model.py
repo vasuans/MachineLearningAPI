2a665c9337f8ca00720a93867a89db75102dd54b
srinivasu addanki <srinivasu.addanki@yahoo.com>
2020-07-03 01:37:10 -0400

    commit to 'master'

---
+++ Model.py
@@ -0,0 +1,40 @@
+# Import dependencies
+import pandas as pd
+import numpy as np
+# Load the dataset in a dataframe object and include only four features as mentioned
+url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+df = pd.read_csv(url)
+include = ['Age', 'Sex', 'Embarked', 'Survived'] # Only four features
+df_ = df[include]
+
+#imputing values for nulls
+categoricals = []
+for col, col_type in df_.dtypes.iteritems():
+     if col_type == 'O':
+          categoricals.append(col)
+     else:
+          df_[col].fillna(0, inplace=True)
+df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
+
+
+from sklearn.linear_model import LogisticRegression
+dependent_variable = 'Survived'
+x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+y = df_ohe[dependent_variable]
+lr = LogisticRegression()
+lr.fit(x, y)
+
+LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+          intercept_scaling=1, max_iter=100, multi_class='ovr', n_jobs=1,
+          penalty='l2', random_state=None, solver='liblinear', tol=0.0001,
+          verbose=0, warm_start=False)
+
+from sklearn.externals import joblib
+joblib.dump(lr, 'model.pkl')
+
+lr = joblib.load('model.pkl')
+
+# Saving the data columns from training
+model_columns = list(x.columns)
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
